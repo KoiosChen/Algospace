1bef7b86855872e66ee3ca593182df6dcf3ebdfc
koioschen <chjz1226@gmail.com>
2021-07-06 14:18:19 +0800

    session

--- app/main/mattermost.py
+++ app/main/mattermost.py
@@ -5,14 +5,8 @@ def bot_hook(hook_url, content):
     headers = {
         'Content-Type': 'application/json'
     }
-    data = {'text': content}
+    data = {'text': content, 'username': 'algospace'}
     print(hook_url, content)
     response = requests.post(hook_url, json=data, headers=headers)
     print(response.text)
     return response
-
-
-if __name__ == "__main__":
-    from app.models import BotHook
-
-    bot_hook(BotHook['Transfer Notification'], "test")
